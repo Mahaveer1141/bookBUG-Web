866dd3c6cd424532b2004b93854b58a2d87167cf
Mahaveer Soni <mahaveer1141@gmail.com>
2023-12-16 22:16:59 +0530

    fixed docker network issue

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:5000";
+export const API_URL = "http://backend-app:5000";
 export const CLIENT_ID =
   "1092901876199-o1q7f9fp0biajsqtbh9jk6n8898412cd.apps.googleusercontent.com";
